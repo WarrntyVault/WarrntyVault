b4fe4a7813443f7aa63ff356b485c0b9bc865b83
iibrahimqurshi <iibrahimalqurshi@stu.kau.edu.sa>
2023-05-07 17:03:08 +0300

    fixing some errors

--- warranty/lib/pages/login_page.dart
+++ warranty/lib/pages/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:warranty/models/_signinBtn.dart';
 import 'package:warranty/pages/getStarted_page.dart';
 import 'package:warranty/pages/register_page.dart';
 
