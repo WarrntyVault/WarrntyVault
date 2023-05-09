d3ddd32b7e351ba6de038304a79d3a9b225e2b23
iibrahimqurshi <iibrahimalqurshi@stu.kau.edu.sa>
2023-05-09 15:21:47 +0300

    Edit some configration files of the app

--- warranty/lib/main.dart
+++ warranty/lib/main.dart
@@ -9,7 +9,6 @@ import 'componats/_BottomNavBarState.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // Replace with actual values
     options: const FirebaseOptions(
         apiKey: "AIzaSyDeQFG_OqqVpAnPu3JlMap9hYT0_zXtZYk",
         appId: "1:1039816346946:android:9169b6408e8d766a1f8cdb",
